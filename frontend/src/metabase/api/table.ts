9f523a8332d46f8493d4dc5625a13cc679d4d5f5
Romeo Van Snick <romeo@romeovansnick.be>
2026-05-19 17:49:52 +0300

    Make sure FormData doesn't get JSON.stringified

--- frontend/src/metabase/api/table.ts
+++ frontend/src/metabase/api/table.ts
@@ -168,9 +168,7 @@ export const tableApi = Api.injectEndpoints({
       query: ({ tableId, formData }) => ({
         method: "POST",
         url: `/api/table/${tableId}/append-csv`,
-        body: { formData },
-        formData: true,
-        fetch: true,
+        body: formData,
       }),
       invalidatesTags: (_, error, { tableId }) =>
         invalidateTags(error, [
@@ -186,9 +184,7 @@ export const tableApi = Api.injectEndpoints({
       query: ({ tableId, formData }) => ({
         method: "POST",
         url: `/api/table/${tableId}/replace-csv`,
-        body: { formData },
-        formData: true,
-        fetch: true,
+        body: formData,
       }),
       invalidatesTags: (_, error, { tableId }) =>
         invalidateTags(error, [
